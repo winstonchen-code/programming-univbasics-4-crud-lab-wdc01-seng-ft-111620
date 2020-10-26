325ef8543e8831732e855208f2c6e8eec3b17497
Winston Chen <winstonchen.wm@gmail.com>
2020-10-26 23:47:38 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,8 +17,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  array = [1, 2]
-  remove = array.pop
+  array = [element]
+  element = array.pop
 end
 
 def remove_element_from_start_of_array(array)
