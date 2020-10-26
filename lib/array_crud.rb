e637dedda5f5aa91dfc6297b3650d7378680250b
Winston Chen <winstonchen.wm@gmail.com>
2020-10-26 23:43:32 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,21 +1,24 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  [1, 2, 3, 4]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = []
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array = []
+  array.unshift (element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array = [1, 2]
+  2 = array.pop
 end
 
 def remove_element_from_start_of_array(array)
