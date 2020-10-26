3e5d89b3d5dc1c33db838ba397d1eca67fb11da3
Winston Chen <winstonchen.wm@gmail.com>
2020-10-26 23:56:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -35,7 +35,8 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array = ["wow", "wowzers"]
+  array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
