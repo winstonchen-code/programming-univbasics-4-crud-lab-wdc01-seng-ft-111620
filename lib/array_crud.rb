d0740563765834f9792fb8bf027743e31f58d17c
Winston Chen <winstonchen.wm@gmail.com>
2020-10-27 00:02:28 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,7 +22,8 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  ["wow", "wowzers"]
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
